697b783418ecf869b018dad677417b81c71694db
Tasya Aditya Rukmana <tadityar@gmail.com>
2017-11-22 16:16:35 +0800

    Change parser format to list of dicts

--- ml.py
+++ ml.py
@@ -6,29 +6,34 @@ def parse_train(filename):
 	pairs = []
 	words = []
 	f = open(filename, 'r')
-	res = {}
+	res = []
+	found = False
 	for k in f:
 		k = k.split()
 		if len(k) > 0:
 			k[0] = k[0].lower()
 			tags.append(k[1])
-			if k[0] not in res:
-				res[k[0]] = {}
-				res[k[0]][k[1]] = 1
-			else:
-				if k[1] not in res[k[0]]:
-					res[k[0]][k[1]] = 1
-				else:
-					res[k[0]][k[1]] += 1
+			for i in res:
+				if k[0] in i:
+					found = True
+					if k[1] not in i[k[0]]:
+						i[k[0]][k[1]] = 1
+					else:
+						i[k[0]][k[1]] += 1
+			if not found:
+				res.append({k[0]: {k[1]: 1}})
+			found = False
 	tags_count = Counter(tags)
 	return res, tags_count
 
-def get_emission_params(words_count, tags_count):
-	words_count_p = copy.deepcopy(words_count)
-	for k, v in words_count_p.items():
-		for i, j in v.items():
-			v[i] = j/tags_count[i]
-	return words_count_p
+# Give emission parameters
+def get_emission_params(wordsCount, tagsCount):
+	wordsCountP = copy.deepcopy(wordsCount)
+	for l in wordsCountP:
+		for k, v in l.items():
+			for i, j in v.items():
+				v[i] = j/tagsCount[i]
+	return wordsCountP
 
 #Given a certain input of most frequent words and their tags, classifies the least N occuring words into a #UNK# unknown field
 def process_unknown_words(f,n):
@@ -109,5 +114,3 @@ def tagging_words(wordAndTag,entiredata):
 testing_data = parser(r'D:\ISTD 2017-2\01-ML\EN\EN\dev.in')
 
 processed_data = process_unknown_words_testing(testing_data,[{"thes" : {"O" : 200}}])
-
-print (processed_data)
