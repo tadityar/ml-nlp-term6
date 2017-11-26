62fd2bab64d2228cafb0f3eda71f99b4e2ffd380
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-11-26 22:46:27 +0800

    fixed error with viterbi recurse thanks @tadityar

--- viterbi.py
+++ viterbi.py
@@ -4,21 +4,17 @@ def viterbi(seq,state,t,e,pre_t=None):
 	elif state < len(seq):
 		return max([viterbi(seq,state+1,t,e,v)*get_transition_param(t,pre_t,v)*get_emission_param(e,seq,state,v) for v in t['tags']])
 	else:
-		out = get_transition_param(t,pre_t,'STOP')
-		return out
+		return get_transition_param(t,pre_t,'STOP')
 
 def get_transition_param(t,u,v):
-	tp = t['map'][t['tags'].index(u)][t['tags'].index(v)]
-	return tp
-
+	return t['map'][t['tags'].index(u)][t['tags'].index(v)]
 
 def get_emission_param(e,seq,state,v):
 	for i in range(len(e)):
 		for key in e[i]:
-			if key == list(seq[state].keys())[0]:
+			if key in list(seq[state].keys())[0]:
 				try:
-					param = e[i][key][v]
-					return param
+					return e[i][key][v]
 				except KeyError as e:
 					return 0
 	return 0
