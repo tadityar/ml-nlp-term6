6c610de6bcc6940e3c1329010166a1f4e090d14c
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-12-03 18:22:02 +0800

    viterbi with erics code

--- viterbi.py
+++ viterbi.py
@@ -7,8 +7,39 @@ class Viterbi:
 	#Please use this call to assign tags to the sequence
 	def assign(self,seq):
 		self.tree = [{} for i in range(len(seq)+1)]
-		return self.__viterbi_backtrack(self.__viterbi_forward(seq,-1)[:-1],'STOP',seq)
+		out = self.__viterbi_backtrack(self.__viterbi_forward(seq,-1)[:-1],'STOP',seq)
+		return out
 		
+		# if state == -1:
+		# 	return self.__viterbi_forward(seq,state+1,'START')
+		# elif state < len(seq):
+		# 	values = []
+		# 	for v in self.t['tags'][1:-1]:
+		# 		lst = self.__get_transition_param(pre_t,v)*self.__get_emission_param(seq,state,v)
+		# 		result = self.tree[state+1][v] if self.tree[state+1].get(v) else self.__viterbi_forward(seq,state+1,v)
+		# 		val = {'val' : result,'tag' : v, 'lst' : lst}
+		# 		self.tree[state][val['tag']] = val['val'] + [val['val'][-1]*val['lst']]
+		# 		values.append(val)				
+
+		# 	o = max(values,key=lambda x:x['val'][-1]*x['lst'])
+		# 	return o['val'] + [o['val'][-1]*o['lst']]
+		# else:
+		# 	self.tree[state][pre_t] = [self.__get_transition_param(pre_t,'STOP')]
+		# 	return self.tree[state][pre]
+
+		##### IMP 2 #####
+		# if state == len(seq):
+		# 	o = max([{'prob' : self.__viterbi_forward(seq,state-1,v), 'cur' : self.__get_transition_param(v,'STOP')} for v in self.t['tags'][1:-1]],key= lambda x:x['prob'][-1]*x['cur'])
+		# 	return o['prob'] + [o['prob'][-1]*o['cur']]
+		# elif state > 0:
+		# 	o =  max([{'prob' : self.tree[state-1][u] if self.tree[state-1].get(u) else self.__viterbi_forward(seq,state-1,u),'cur' : self.__get_transition_param(u,pre_t)*self.__get_emission_param(seq,state,pre_t)} for u in self.t['tags'][1:-1]],key= lambda x:x['prob'])
+		# 	self.tree[state][pre_t] = o['prob'] + [o['prob'][-1]*o['cur']]
+		# 	return o['prob'] + [o['prob'][-1]*o['cur']]
+		# else:
+		# 	self.tree[state][pre_t] = [self.__get_transition_param('START',pre_t)*self.__get_emission_param(seq,state,pre_t)]
+		# 	return self.tree[state][pre_t]
+
+		###### IMP 3 #######
 	def __viterbi_forward(self,seq,state,pre_t=None):
 		if state == -1:
 			return self.__viterbi_forward(seq,state+1,'START')
@@ -27,18 +58,22 @@ class Viterbi:
 			self.tree[state][pre_t] = [self.__get_transition_param(pre_t,'STOP')]
 			return [self.__get_transition_param(pre_t,'STOP')]
 
+
 	def __get_transition_param(self,u,v):
 		return self.t['map'][self.t['tags'].index(u)][self.t['tags'].index(v)]
 
-	def __get_emission_param(self,seq,state,v):
-		for i in range(len(self.e)):
-			for key in self.e[i]:
-				if key in list(seq[state].keys())[0]:
-					try:
-						return self.e[i][key][v]
-					except KeyError as er:
-						return self.e[0]["#UNK#"][v]
-		return self.e[0]["#UNK#"][v]
+	def __get_emission_param(self, seq, state, tag):
+		word = list(seq[state].keys())[0]
+		if word in self.e:
+			if tag in self.e[word]:
+				return self.e[word][tag]
+			else:
+				if tag == 'START' or tag == 'STOP':
+					return 0
+				return self.e["#UNK#"][tag]
+		if tag == 'START' or tag == 'STOP':
+			return 0
+		return self.e["#UNK#"][tag]
 
 	def __viterbi_backtrack(self,pi,post,seq):
 		if len(pi) == 0:
